ad1733ae5cf1f07804ddc18322cc03bcaea20a3b
MartinIsoz <isozmartin@gmail.com>
2025-07-15 14:11:16 +0200

    Debugging of the updated compile scripts and addition of the possibility to compile without cleaning (wmake without wclean)

--- compileAll.sh
+++ compileAll.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
+if [ "$1" = "clean" ]; then
+    do_clean=true
+else
+    do_clean=false
+fi
+
 rootDir=$(pwd)
 
 echo "Working from directory: $rootDir"
 
 # compile the library
+echo "compiling the library"
 cd src/HFDIBDEM
-wclean
+if [ "$do_clean" = true ]; then
+    wclean
+fi
 wmake libso
 cd $rootDir
 
 # directories to skip during compilation
-skip_dirs="VOFHFDIB multiphaseInterHFDIBFoam"
+skip_dirs="VoFHFDIB multiphaseInterHFDIBFoam"
 
 # compile the solvers
 cd applications/solvers
 for pd in ./*/ ; do
     [ -L "${pd%/}" ] && continue
+    echo "compiling solvers in $pd"
     cd $pd
     for sd in */ ; do
         [ -L "${sd%/}" ] && continue
@@ -26,8 +36,11 @@ for pd in ./*/ ; do
                 continue 2
             fi
         done
+        echo "... solver: $dir_name"
         cd $sd
-        wclean
+        if [ "$do_clean" = true ]; then
+            wclean
+        fi
         wmake
         cd ..
     done
--- compileSol.sh
+++ compileSol.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+if [ "$1" = "clean" ]; then
+    do_clean=true
+else
+    do_clean=false
+fi
+
 rootDir=$(pwd)
 
 echo "Working from directory: $rootDir"
 
 # directories to skip during compilation
-skip_dirs="VOFHFDIB multiphaseInterHFDIBFoam"
+skip_dirs="VoFHFDIB multiphaseInterHFDIBFoam"
 
 # compile the solvers
 cd applications/solvers
 for pd in ./*/ ; do
     [ -L "${pd%/}" ] && continue
+    echo "compiling solvers in $pd"
     cd $pd
     for sd in */ ; do
         [ -L "${sd%/}" ] && continue
@@ -20,8 +27,11 @@ for pd in ./*/ ; do
                 continue 2
             fi
         done
+        echo "... solver: $dir_name"
         cd $sd
-        wclean
+        if [ "$do_clean" = true ]; then
+            wclean
+        fi
         wmake
         cd ..
     done
